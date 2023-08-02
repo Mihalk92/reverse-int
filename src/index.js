ae56f5a0677909129388c86715abcbca9ca84194
Dmitry Kovalenko <mihalk9292@gmail.com>
2023-08-02 10:15:26 +0300

    init: start task revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n * (-1);
+    let numToArr = n.toString().split('');
+    let arrRevers = numToArr.reverse();
+    let arrToNum = +arrRevers.join('');
+    return arrToNum;
+  } else {
+    let numToArr = n.toString().split('');
+    let arrRevers = numToArr.reverse();
+    let arrToNum = +arrRevers.join('');
+    return arrToNum;
+  }
 }
